4e73743edb92a756b85429ac5645b06195f391a5
MasudRana15924 <masudrana15924@outlook.com>
2024-03-10 00:01:49 +0600

    profile update

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const {
-    createOTP,
     studentRegistration,
     studentLogin,
     changePassword
@@ -8,7 +7,6 @@ const {
 const { studentAuthenticate } = require("../middleware/authenticate");
 const { findBookingByStudentId } = require("../controllers/bookteacher.controller");
 const router = Router();
-router.post("/send/otp", createOTP);
 router.post("/student/register", studentRegistration);
 router.post("/student/login", studentLogin);
 router.post("/student/change-password",studentAuthenticate,changePassword);
